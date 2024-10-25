26d18ed8f88b428e14cc7ca2ff527daf2d1086e0
Julian Mikkelsen <juliansoto04@gmail.com>
2024-10-24 18:27:18 -0600

    Added file upload logic

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -16,3 +16,31 @@ ALLOWED_EXTENSIONS = {'dvw'}
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/')
+def home():
+    return render_template('upload.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    # Check if the post request has the file part
+    if 'file' not in request.files:
+        return 'No file part'
+    
+    file = request.files['file']
+    
+    # If user does not select file, browser also submits an empty part without filename 
+    if file.filename == '':
+        return 'No selected file'
+    
+    # If file is allowed, save it to the upload folder
+    if file and allowed_file(file.filename):
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(file_path)
+        
+        return 'File uploaded successfully'
+    
+    return 'Invalid file type'
+
+if  __name__ == '__main__':
+    app.run(debug=True)
+    
